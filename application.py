35003958ebd77b9ebe94c137a51f676080d73cc3
fadhilmufid <mufidfadhil@gmail.com>
2022-03-16 20:46:33 +0700

    Add project root with import os

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello, world"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
+
+project_root = os.path.dirname(__file__)
+template_path = os.path.join(project_root, '/templates')
+app = Flask(__name__, template_folder=template_path)
