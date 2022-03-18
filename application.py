34463a45674c438c59a6f4e7d2067186d1d52d76
fadhilmufid <mufidfadhil@gmail.com>
2022-03-18 15:23:17 +0700

    Add Javascript Alert

--- application.py
+++ application.py
@@ -21,13 +21,11 @@ def registrered():
 @app.route("/register", methods=["POST"])
 def register():
     #TODO
-    if not request.form.get("name") or not request.form.get("dorm"):
-        return render_template("failure.html")
     file = open("registered.csv", "a")
     writer = csv.writer(file)
     writer.writerow((request.form.get("name"),request.form.get("dorm")))
     file.close()
-    return render_template("success.html")
+    return redirect("/registered")
 
 
 if __name__ == "__main__":
